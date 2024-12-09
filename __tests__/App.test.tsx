9f9cd4f7c31e2b229e5c0f03813142f85e46beec
chan9yu <dev.cgyeo@gmail.com>
2024-12-09 12:57:14 +0900

    feat: init project

---
+++ __tests__/App.test.tsx
@@ -0,0 +1,10 @@
+import { it } from '@jest/globals';
+import React from 'react';
+import 'react-native';
+import renderer from 'react-test-renderer';
+
+import App from '../src/App';
+
+it('renders correctly', () => {
+	renderer.create(<App />);
+});
